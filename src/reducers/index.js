60ed9e46b59b5417dd8adb89cc579d93ccc9bd10
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-09-20 20:51:55 +0530

    doc: index added for reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import users from "./user";
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  users,
+});
+
+export default reducers;
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
-import users from "../reducers/user";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "../sagas/rootSaga";
+import reducers from "../reducers";
 
 
 
@@ -9,6 +9,6 @@ const sagaMiddleware = createSagaMiddleware();
 
 // const middleware = [sagaMiddleware];
 
-export const store = createStore(users, applyMiddleware(sagaMiddleware))
+export const store = createStore(reducers, applyMiddleware(sagaMiddleware))
 
 sagaMiddleware.run(rootSaga);
